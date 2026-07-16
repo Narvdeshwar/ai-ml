6cc523d9c5059a648d0349a7fa432bdde21c7085
narvdeshwar <sam.07soni@gmail.com>
2026-07-16 07:07:44 +0000

    more method added for dict

--- python-basics/dict.py
+++ python-basics/dict.py
@@ -6,3 +6,16 @@ dict={
 print(dict)
 
 # dictionary are mutable and unordered.
+all_keys=dict.keys()
+print(f"All keys of dict {all_keys}")
+
+all_values=dict.values()
+print(f"All values of dict {all_values}")
+
+# print(dict.items())
+print(f"key value pair print {dict.items()}")
+
+print(dict.get("age"))
+
+print(dict.update({"fnae":"ravi"}))
+print(dict)
