0e6768bccc9b6f59843d12c0c4144b59fe11d7c9
narvdeshwar <sam.07soni@gmail.com>
2026-07-16 07:19:18 +0000

    set

---
+++ python-basics/set.py
@@ -0,0 +1,9 @@
+set_val={1,2,3,4,5,4,5,6}
+print(set_val)
+print(f"type: {type(set_val)}")
+
+empty={} # by default it will be dict then we have to use the set method to convert this into set
+print(f"type: {type(empty)}")
+
+empty_set=set()
+print(f"type: {type(empty_set)}")
