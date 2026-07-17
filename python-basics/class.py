e2872847d6d41244152f1a17f3ef3144446e0f68
narvdeshwar <sam.07soni@gmail.com>
2026-07-17 08:24:47 +0000

    added static method

--- python-basics/class.py
+++ python-basics/class.py
@@ -11,10 +11,16 @@ class Laptop:
     def get_info(self):
         print(f"Laptop has {self.ram} RAM with {self.storage}GB {self.storage_type}")
 
+    @staticmethod
+    def get_dicounted_price(price,discount_percentage):
+        final_price=price-(price*discount_percentage/100)
+        print(f"Final discounted price = {final_price}")
+
 l1=Laptop("16","512")
 l2=Laptop("8","256")
 l1.get_storage_type()
 # l1.get_info()
+l1.get_dicounted_price(80_000,10)
 
 
 ######################################################
@@ -26,4 +32,8 @@ l1.get_storage_type()
 # 1. it has first parameter `cls`
 # 2. it can only access the class attributes
 # 3. it has decorators called as `@classmethod` so the class method can be accessed from the object
+
+# static method
+# 1. It doesn't have any `self` or `cls` parameter
+# 2. It has decorators called as `@staticmethod`
 ######################################################
