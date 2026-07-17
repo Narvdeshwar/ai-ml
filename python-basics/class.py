00f04d5db658968dd91059fd4da9db3677b1ca79
narvdeshwar <sam.07soni@gmail.com>
2026-07-17 07:33:52 +0000

    class explainer

--- python-basics/class.py
+++ python-basics/class.py
@@ -4,12 +4,17 @@ class Laptop:
         self.ram=ram
         self.storage=storage
 
+    @classmethod
+    def get_storage_type(cls):
+        print(f"Laptop has storage type {cls.storage_type}")
+
     def get_info(self):
         print(f"Laptop has {self.ram} RAM with {self.storage}GB {self.storage_type}")
 
 l1=Laptop("16","512")
 l2=Laptop("8","256")
-l1.get_info()
+l1.get_storage_type()
+# l1.get_info()
 
 
 ######################################################
@@ -17,5 +22,8 @@ l1.get_info()
 # 1. It has the first parameter `self`
 # 2. It can access both the class and instance attributes
 
-
+# Class
+# 1. it has first parameter `cls`
+# 2. it can only access the class attributes
+# 3. it has decorators called as `@classmethod` so the class method can be accessed from the object
 ######################################################
