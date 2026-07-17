a5ad4ba6987de2b3ccf2d743955522c30a445220
narvdeshwar <sam.07soni@gmail.com>
2026-07-17 07:10:17 +0000

    instance method

--- python-basics/class.py
+++ python-basics/class.py
@@ -1,5 +1,21 @@
-class Student:
-    def __init__(self):
-        print("Student constructor is called")
+class Laptop:
+    storage_type="ssd"
+    def __init__(self,ram,storage):
+        self.ram=ram
+        self.storage=storage
 
-s1=Student()
+    def get_info(self):
+        print(f"Laptop has {self.ram} RAM with {self.storage}GB {self.storage_type}")
+
+l1=Laptop("16","512")
+l2=Laptop("8","256")
+l1.get_info()
+
+
+######################################################
+# Instance:
+# 1. It has the first parameter `self`
+# 2. It can access both the class and instance attributes
+
+
+######################################################
