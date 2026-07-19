99a53f0d5277287790dc1c195e1d93e41f4e78b6
narvdeshwar <sam.07soni@gmail.com>
2026-07-19 11:52:03 +0000

    abstract class

---
+++ python-basics/abstract.py
@@ -0,0 +1,24 @@
+from abc import ABC,abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Lion(Animal):
+    def make_sound(self):
+        print("Roar !")
+
+class Cow(Animal):
+    def make_sound(self):
+        print("Moo!")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("Meaw!!")
+
+lion=Lion()
+lion.make_sound()
+cow=Cow()
+cow.make_sound()
+cat=Cat()
+cat.make_sound()
