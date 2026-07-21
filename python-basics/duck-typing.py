9227cd4a29be5af0372b1121126b850140efd415
narvdeshwar <sam.07soni@gmail.com>
2026-07-21 11:03:02 +0000

    duck typing

---
+++ python-basics/duck-typing.py
@@ -0,0 +1,14 @@
+class Teacher:
+    def get_teacher_role(self):
+        print(f"role is teacher")
+
+class Assitant:
+    def get_assitant_role(self):
+        print(f"role is assitant")
+
+t1=Teacher()
+t1.get_teacher_role()
+
+a1=Assitant()
+a1.get_assitant_role()
+
